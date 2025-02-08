17cbc552581f796ee53320ce366eae385f820b06
AzaanTechzilla <azaan.khalid@techzillaglobal.com>
2025-02-08 19:06:09 +0500

    changes done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,48 +1,55 @@
-import React from 'react'
+import React from 'react';
 import "./Footer.css";
-import { FaHome, FaInstagram,FaFacebook, FaMailBulk, FaPhone, FaTwitter } from 'react-icons/fa';
+import { FaHome, FaInstagram, FaFacebook, FaMailBulk, FaPhone, FaTwitter, FaLinkedin } from 'react-icons/fa';
+
 const Footer = () => {
   return (
     <>
-   <div className="footer">
-    <div className="footer-container">
-        <div className="left">
+      <div className="footer">
+        <div className="footer-container">
+          <div className="left">
             <div className="location">
-                <FaHome size={20} style={{color:"white",marginRight:"2rem"}} />
-                <div>
-                    <p>House no 273/A Street no 20 Area Javaid Colony, Rashid Pura, </p>
-                    <p>Lahore</p>
-                </div>
+              <FaHome size={20} style={{ color: "white", marginRight: "2rem" }} />
+              <div>
+                <p>House no 273/A Street no 20 Area Javaid Colony, Rashid Pura, </p>
+                <p>Lahore</p>
+              </div>
             </div>
             <div className="phone">
-                <h4>
-
-                <FaPhone size={20} style={{color:"white",marginRight:"2rem"}} />
+              <h4>
+                <FaPhone size={20} style={{ color: "white", marginRight: "2rem" }} />
                 03334490739
-                </h4>
+              </h4>
             </div>
             <div className="email">
-                
-                <h4>
- <FaMailBulk size={20} style={{color:"white",marginRight:"2rem"}} />
+              <h4>
+                <FaMailBulk size={20} style={{ color: "white", marginRight: "2rem" }} />
                 rajaazan918@gmail.com
-                </h4>
-                
+              </h4>
             </div>
-        </div>
-        <div className="right">
+          </div>
+          <div className="right">
             <h4>About the Company</h4>
             <p>I am A Fresher. I Love to discuss on Projects</p>
             <div className="social">
-          <FaFacebook size={30} style={{color:"white",marginRight:"1rem"}} />
-            <FaTwitter size={30} style={{color:"white",marginRight:"1rem"}} />
-            <FaInstagram size={30} style={{color:"white",marginRight:"1rem"}} />
+              <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                <FaFacebook size={30} style={{ color: "white", marginRight: "1rem" }} />
+              </a>
+              <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                <FaTwitter size={30} style={{ color: "white", marginRight: "1rem" }} />
+              </a>
+              <a href="https://www.instagram.com/x__rajaazan__x/" target="_blank" rel="noopener noreferrer">
+                <FaInstagram size={30} style={{ color: "white", marginRight: "1rem" }} />
+              </a>
+              <a href="https://www.linkedin.com/in/raja-azaan-746635279/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin size={30} style={{ color: "white", marginRight: "1rem", cursor: 'pointer' }} />
+              </a>
             </div>
+          </div>
         </div>
-    </div>
-   </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Components/Heroimage.js
+++ src/Components/Heroimage.js
@@ -11,8 +11,8 @@ const Heroimage = () => {
             <img className='intro-img' src={introimg} alt="introimg" />
         </div>
         <div className="content">
-            <p>HI,I'M A BEGINNER REACT DEVELOPER</p>
-            <h1>React Developer.</h1>
+            <p>HI,I'M A DEVELOPER </p>
+            <h1>Frontend Developer.</h1>
             <div>
                <Link to="/project" className='btn'>PROJECTS</Link>
                <Link to="/contact" className='btn btn-light'>CONTACT</Link>
